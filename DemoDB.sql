1022319b056d4f7abb1ad3bf45e437b706575639
SiMiezz <simone.giordano5@studenti.unina.it>
2021-12-27 22:26:41 +0100

    sistemato trigger lezioni

--- DemoDB.sql
+++ DemoDB.sql
@@ -58,7 +58,7 @@ CREATE TABLE `corsoformazione` (
   PRIMARY KEY (`idcorso`),
   KEY `Fk_op_idx` (`id`),
   CONSTRAINT `Fk_op` FOREIGN KEY (`id`) REFERENCES `operatore` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `corsoformazione` (
 
 LOCK TABLES `corsoformazione` WRITE;
 /*!40000 ALTER TABLE `corsoformazione` DISABLE KEYS */;
-INSERT INTO `corsoformazione` VALUES (30,'informatica','word',50,120,'1155'),(31,'italiano','letteratura',40,150,'1155'),(32,'programmazione','java',30,120,'1155'),(33,'algebra','polinomi',30,150,'1255'),(34,'analisi','limiti',50,250,'1255');
+INSERT INTO `corsoformazione` VALUES (36,'informatica','word',20,130,'1155'),(37,'italiano','letteratura',40,120,'1255');
 /*!40000 ALTER TABLE `corsoformazione` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,7 +97,6 @@ CREATE TABLE `iscritto` (
 
 LOCK TABLES `iscritto` WRITE;
 /*!40000 ALTER TABLE `iscritto` DISABLE KEYS */;
-INSERT INTO `iscritto` VALUES ('2021-12-27','13:14:16','6600',30),('2021-12-27','13:14:13','6600',32),('2021-12-27','13:14:10','6600',34),('2021-12-27','13:15:15','6615',31),('2021-12-27','13:15:17','6615',33);
 /*!40000 ALTER TABLE `iscritto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,7 +118,7 @@ CREATE TABLE `lezione` (
   PRIMARY KEY (`idlezione`),
   KEY `Fk_corsi_idx` (`idcorso`),
   CONSTRAINT `Fk_corsi` FOREIGN KEY (`idcorso`) REFERENCES `corsoformazione` (`idcorso`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +127,7 @@ CREATE TABLE `lezione` (
 
 LOCK TABLES `lezione` WRITE;
 /*!40000 ALTER TABLE `lezione` DISABLE KEYS */;
-INSERT INTO `lezione` VALUES (10,'lezione 1','lezione informatica','02:00:00','2021-12-27','12:41:58',30),(11,'lezione 2','lezione informatica','02:00:00','2021-12-27','12:41:58',30),(12,'lezione 1','lezione italiano','02:00:00','2021-12-27','12:44:46',31),(13,'lezione 2','lezione italiano','02:00:00','2021-12-27','12:44:46',31),(14,'lezione 3','lezione italiano','02:00:00','2021-12-27','12:44:46',31),(15,'lezione 4','lezione italiano','02:00:00','2021-12-27','12:44:46',31),(16,'lezione 1','lezione programmazione','02:00:00','2021-12-27','12:45:05',32),(17,'lezione 2','lezione programmazione','02:00:00','2021-12-27','12:45:05',32),(18,'lezione 3','lezione programmazione','02:00:00','2021-12-27','12:45:05',32),(19,'lezione 4','lezione programmazione','02:00:00','2021-12-27','12:45:05',32),(20,'lezione 5','lezione programmazione','02:00:00','2021-12-27','12:45:05',32),(21,'lezione 6','lezione programmazione','02:00:00','2021-12-27','12:45:05',32),(22,'lezione 1','lezione algebra','02:00:00','2021-12-27','12:45:29',33),(23,'lezione 2','lezione algebra','02:00:00','2021-12-27','12:45:29',33),(24,'lezione 3','lezione algebra','02:00:00','2021-12-27','12:45:29',33),(25,'lezione 4','lezione algebra','02:00:00','2021-12-27','12:45:29',33),(26,'lezione 1','lezione analisi','02:00:00','2021-12-27','12:45:38',34),(27,'lezione 2','lezione analisi','02:00:00','2021-12-27','12:45:38',34),(28,'lezione 3','lezione analisi','02:00:00','2021-12-27','12:45:38',34),(29,'lezione 4','lezione analisi','02:00:00','2021-12-27','12:45:38',34);
+INSERT INTO `lezione` VALUES (34,'lezione 1','lezione informatica','02:00:00','2021-12-28','15:00:00',36),(35,'lezione 2','lezione informatica','02:00:00','2021-12-29','15:00:00',36),(36,'lezione 1','lezione italiano','02:00:00','2021-12-28','11:00:00',37),(37,'lezione 2','lezione italiano','02:00:00','2021-12-29','16:00:00',37);
 /*!40000 ALTER TABLE `lezione` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -228,4 +227,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-27 21:49:19
+-- Dump completed on 2021-12-27 22:26:22
