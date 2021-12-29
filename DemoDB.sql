72ff1403ec1ffb507778f1dfb8843653e4dffd88
SiMiezz <simone.giordano5@studenti.unina.it>
2021-12-29 20:25:11 +0100

    self contained

--- DemoDB.sql
+++ DemoDB.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `demodb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `demodb`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: demodb
@@ -256,4 +258,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-29 20:00:12
+-- Dump completed on 2021-12-29 20:24:34
