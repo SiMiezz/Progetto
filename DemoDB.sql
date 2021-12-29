2dec3de2531a25ba1f78334185a183aa67a99b84
SiMiezz <simone.giordano5@studenti.unina.it>
2021-12-29 12:50:42 +0100

    eliminato data ed ora iscritto e partecipa

--- DemoDB.sql
+++ DemoDB.sql
@@ -26,6 +26,7 @@ CREATE TABLE `areetematiche` (
   `tipo` varchar(30) NOT NULL,
   `descrizione` text,
   `idcorso` int NOT NULL,
+  UNIQUE KEY `tipo_UNIQUE` (`tipo`),
   KEY `Fk_corso_idx` (`idcorso`),
   KEY `Fk_corsoformazione_idx` (`idcorso`),
   CONSTRAINT `Fk_corsoformazione` FOREIGN KEY (`idcorso`) REFERENCES `corsoformazione` (`idcorso`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -38,6 +39,7 @@ CREATE TABLE `areetematiche` (
 
 LOCK TABLES `areetematiche` WRITE;
 /*!40000 ALTER TABLE `areetematiche` DISABLE KEYS */;
+INSERT INTO `areetematiche` VALUES ('informazione','studio',43),('matematica','',46);
 /*!40000 ALTER TABLE `areetematiche` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +60,7 @@ CREATE TABLE `corsoformazione` (
   PRIMARY KEY (`idcorso`),
   KEY `Fk_op_idx` (`id`),
   CONSTRAINT `Fk_op` FOREIGN KEY (`id`) REFERENCES `operatore` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +69,7 @@ CREATE TABLE `corsoformazione` (
 
 LOCK TABLES `corsoformazione` WRITE;
 /*!40000 ALTER TABLE `corsoformazione` DISABLE KEYS */;
-INSERT INTO `corsoformazione` VALUES (36,'informatica','word',20,130,'1155'),(37,'italiano','letteratura',40,120,'1255');
+INSERT INTO `corsoformazione` VALUES (43,'informatica','word',30,120,'1155'),(44,'programmazione','java',10,150,'1155'),(45,'analisi','limiti',40,200,'1155'),(46,'algebra','polinomi',40,120,'1155'),(47,'ade','circuiti',40,120,'1255'),(48,'laboratorio','c',50,100,'1255'),(49,'algoritmi','',30,120,'1255');
 /*!40000 ALTER TABLE `corsoformazione` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,8 +81,6 @@ DROP TABLE IF EXISTS `iscritto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `iscritto` (
-  `data` date NOT NULL,
-  `orario` time DEFAULT NULL,
   `matricola` varchar(15) NOT NULL,
   `idcorso` int NOT NULL,
   UNIQUE KEY `stud_UNIQUE` (`matricola`,`idcorso`),
@@ -97,6 +97,7 @@ CREATE TABLE `iscritto` (
 
 LOCK TABLES `iscritto` WRITE;
 /*!40000 ALTER TABLE `iscritto` DISABLE KEYS */;
+INSERT INTO `iscritto` VALUES ('6600',43),('6615',43),('6615',44),('6600',46),('6600',47),('6615',47),('6600',48),('6615',48),('6600',49),('6615',49);
 /*!40000 ALTER TABLE `iscritto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,7 +119,7 @@ CREATE TABLE `lezione` (
   PRIMARY KEY (`idlezione`),
   KEY `Fk_corsi_idx` (`idcorso`),
   CONSTRAINT `Fk_corsi` FOREIGN KEY (`idcorso`) REFERENCES `corsoformazione` (`idcorso`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +128,7 @@ CREATE TABLE `lezione` (
 
 LOCK TABLES `lezione` WRITE;
 /*!40000 ALTER TABLE `lezione` DISABLE KEYS */;
-INSERT INTO `lezione` VALUES (34,'lezione 1','lezione informatica','02:00:00','2021-12-28','15:00:00',36),(35,'lezione 2','lezione informatica','02:00:00','2021-12-29','15:00:00',36),(36,'lezione 1','lezione italiano','02:00:00','2021-12-28','11:00:00',37),(37,'lezione 2','lezione italiano','02:00:00','2021-12-29','16:00:00',37);
+INSERT INTO `lezione` VALUES (49,'lezione 1','lezione informatica','02:00:00','2021-12-28','17:00:00',43),(50,'lezione 2','lezione informatica','02:00:00','2021-12-29','15:00:00',43),(51,'lezione 3','lezione informatica','02:00:00','2021-12-30','09:00:00',43),(52,'lezione 4','lezione informatica','02:00:00','2021-12-31','09:00:00',43),(53,'lezione 5','lezione informatica','02:00:00','2022-01-01','14:00:00',43),(54,'lezione 1','lezione programmazione','02:00:00','2021-12-28','13:00:00',44),(55,'lezione 2','lezione programmazione','02:00:00','2021-12-29','15:00:00',44),(56,'lezione 3','lezione programmazione','02:00:00','2021-12-30','14:00:00',44),(57,'lezione 4','lezione programmazione','02:00:00','2021-12-31','13:00:00',44),(58,'lezione 5','lezione programmazione','02:00:00','2022-01-01','16:00:00',44),(59,'lezione 1','lezione analisi','02:00:00','2021-12-28','09:00:00',45),(60,'lezione 2','lezione analisi','02:00:00','2021-12-29','17:00:00',45),(61,'lezione 3','lezione analisi','02:00:00','2021-12-30','10:00:00',45),(62,'lezione 4','lezione analisi','02:00:00','2021-12-31','18:00:00',45),(63,'lezione 1','lezione algebra','02:00:00','2021-12-28','12:00:00',46),(64,'lezione 2','lezione algebra','02:00:00','2021-12-29','14:00:00',46),(65,'lezione 3','lezione algebra','02:00:00','2021-12-30','17:00:00',46),(66,'lezione 1','lezione ade','02:00:00','2021-12-28','12:00:00',47),(67,'lezione 2','lezione ade','02:00:00','2021-12-29','12:00:00',47),(68,'lezione 3','lezione ade','02:00:00','2021-12-30','09:00:00',47),(69,'lezione 1','lezione laboratorio','02:00:00','2021-12-28','15:00:00',48),(70,'lezione 2','lezione laboratorio','02:00:00','2021-12-29','09:00:00',48),(71,'lezione 1','lezione algoritmi','02:00:00','2021-12-28','12:00:00',49),(72,'lezione 2','lezione algoritmi','02:00:00','2021-12-29','14:00:00',49),(73,'lezione 3','lezione algoritmi','02:00:00','2021-12-30','11:00:00',49);
 /*!40000 ALTER TABLE `lezione` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -168,8 +169,6 @@ DROP TABLE IF EXISTS `partecipa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `partecipa` (
-  `data` date NOT NULL,
-  `orario` time DEFAULT NULL,
   `matricola` varchar(15) NOT NULL,
   `idlezione` int NOT NULL,
   UNIQUE KEY `Partecipa_UNIQUE` (`idlezione`,`matricola`),
@@ -186,6 +185,7 @@ CREATE TABLE `partecipa` (
 
 LOCK TABLES `partecipa` WRITE;
 /*!40000 ALTER TABLE `partecipa` DISABLE KEYS */;
+INSERT INTO `partecipa` VALUES ('6615',49),('6600',50),('6600',51),('6600',52),('6615',55),('6600',64),('6615',68),('6600',69),('6615',70),('6615',71),('6600',72),('6615',72),('6600',73),('6615',73);
 /*!40000 ALTER TABLE `partecipa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -227,4 +227,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-27 22:26:22
+-- Dump completed on 2021-12-29 12:50:28
